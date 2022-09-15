f7886dab9b35b206704df9b6694aff6269ad6459
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-09-15 10:38:10 +0200

    Update get_cookie.py

--- intelwatcher/get_cookie.py
+++ intelwatcher/get_cookie.py
@@ -109,7 +109,7 @@ def selenium_cookie(config, log):
         os.remove(f)
 
     user_agent = ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                  ' Chrome/60.0.3112.50 Safari/537.36')
+                  ' Chrome/105.0.5195.102 Safari/537.36')
 
     if config.webdriver == 'firefox':
         options = webdriver.FirefoxOptions()
